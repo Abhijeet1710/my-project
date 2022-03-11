d5f2cc19a1a582f50ab2040bfd32bd8704fc4990
= <=>
2022-03-11 23:23:55 +0530

    working web3 + tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Helloabi from "./abi/Hello.json";
+import Web3 from "web3";
+import './index.css';
 
 function App() {
+
+  const [refresh, setrefresh] = useState(0);
+  const [loading2, setloading2] = useState(false);
+  const [account, setAccount] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [methodOp, setMethodOp] = useState("Click Here");
+  const [deployedContract, setContract] = useState();
+
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      await window.ethereum.enable();
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  };
+
+  const loadBlockchainData = async () => {
+    setLoading(true);
+    if (typeof window.ethereum == "undefined" ) {
+      return;
+    }
+    
+    const web3 = new Web3(window.ethereum);
+
+    let url = window.location.href;
+    const accounts = await web3.eth.getAccounts();
+    setAccount(accounts[0]);
+
+    if (accounts.length == 0) {
+      return;
+    }
+    const networkId = await web3.eth.net.getId();
+    let deployedNetwork = Helloabi.networks[networkId];
+    
+    try {
+      const ContractInstance = new web3.eth.Contract(
+        Helloabi.abi,
+        deployedNetwork && deployedNetwork.address
+      );
+      
+      setContract(ContractInstance);
+      console.log(ContractInstance);
+      
+      // const onr = await ContractInstance.methods.getA().call();
+      // setMethodOp(onr);
+
+      setLoading(false);
+    } catch {
+      window.alert("the contract not deployed to detected network.");
+      setloading2(true);
+    }
+  };
+
+  const setMsg = async() => {
+      const onr = await deployedContract.methods.getA().call();
+      setMethodOp(onr);
+  }
+
+  useEffect(() => {
+    loadWeb3();
+    loadBlockchainData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-orange-500" onClick={ setMsg }> { methodOp } </h1>
     </div>
   );
 }
